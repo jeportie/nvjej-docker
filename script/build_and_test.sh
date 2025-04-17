7b413d3a81176bb20da9e5e0cfe426eb99b78981
jeportie <jeportie@student.42.fr>
2025-04-17 09:38:05 +0000

    push pause

--- script/build_and_test.sh
+++ script/build_and_test.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Exit immediately if a command exits with a non-zero status (except in tests)
-set -o errexit
-
 # Create and enter build directory
 mkdir -p build
 cd build || { echo "Failed to enter build directory"; exit 1; }
@@ -13,11 +10,15 @@ cmake .. && make
 # Run tests and handle results
 if ctest; then
     echo "\nAll tests passed.\n"
+	cd ..
+	rm -rf build
 else
 	echo "Tests failed."
 	ctest --rerun-failed --output-on-failure
+	cd ..
+	rm -rf build
 fi
 
 # Return to project root and clean up
-cd ..
-rm -rf build
+# cd ..
+# rm -rf build
